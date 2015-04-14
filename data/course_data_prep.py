3624f7070d2507cfba601e2cd170ebc21a7237d8
samarth <bansalsamarth@yahoo.com>
2015-04-14 20:08:10 +0530

    Course data modified

--- data/course_data_prep.py
+++ data/course_data_prep.py
@@ -5,12 +5,23 @@ f = open('data_course_students.txt')
 courses = {}
 
 for i in f:
-    d = i.split(",")
-    try:
-        d.remove('\r\n')
-    except:
-        pass
-    courses[d[0]] = d[1:]
+	d = i.split(",")
+	try:
+		d.remove('\r\n')
+	except:
+		pass
+	try:
+		if d[0][-1] == "A":
+			c = d[0][:-1]
+		else:
+			c = d[0]
+
+		if c in courses.keys():
+			courses[c] += d[1:]
+		else:
+			courses[c] = d[1:]
+	except:
+		pass
 
 data = json.dumps(courses)
 
